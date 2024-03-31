3f530e40f8e8c1b83b0a66cb4ae1ce5f4347ca12
v-plotnikov <villagerfeed@yandex.ru>
2024-03-31 21:21:19 +0500

    Описание конструкторов шаблонов классов List и ListNode

--- main.cpp
+++ main.cpp
@@ -1,7 +1,40 @@
 #include <iostream>
 
-int main()
-{
-    std::cout << "Hello, World!" << std::endl;
+using namespace std;
+
+template<typename Type>
+class ListNode {
+public:
+    ListNode<Type> *next;
+    ListNode<Type> *prev;
+    Type data;
+
+    ListNode(Type data) {
+        this->data = data;
+        next = nullptr;
+        prev = nullptr;
+    }
+};
+
+template<typename Type>
+class List {
+private:
+    ListNode<Type> *head;
+    ListNode<Type> *tail;
+    int size;
+public:
+    List<Type>() {
+        head = nullptr;
+        tail = nullptr;
+        size = 0;
+    };
+
+    int getSize() {
+        return size;
+    }
+};
+
+int main() {
+    List<int> list;
     return 0;
 }
