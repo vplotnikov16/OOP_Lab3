da6ea266d7eeccb6700dc0a3cf3bfad9417f1ddb
v-plotnikov <villagerfeed@yandex.ru>
2024-03-31 21:34:25 +0500

    Добавлена возможность обращаться к элементам списка по индексу через квадратные скобки

--- main.cpp
+++ main.cpp
@@ -32,9 +32,45 @@ public:
     int getSize() {
         return size;
     }
+
+    void push_back(Type value) {
+        ListNode<Type> *node = new ListNode<Type>(value);
+        if (size == 0) head = tail = node;
+        else {
+            tail->next = node;
+            node->prev = tail;
+            tail = node;
+        }
+        size++;
+    }
+
+    Type &operator[](const int index) {
+        int i = 0;
+        if (index > size / 2) {
+            ListNode<Type> *current = tail;
+            while (i != size - index - 1) {
+                current = current->prev;
+                i++;
+            }
+            return current->data;
+        } else {
+            ListNode<Type> *current = head;
+            while (i != index) {
+                current = current->next;
+                i++;
+            }
+            return current->data;
+        }
+    }
 };
 
 int main() {
     List<int> list;
+    list.push_back(0);
+    list.push_back(4);
+    list.push_back(-9);
+    cout << list.getSize() << endl;
+    list.push_back(11);
+    cout << list[3];
     return 0;
 }
